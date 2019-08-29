bd9dafaa626068c5a928b5abe4735f3dd3ea4f23
smyers24 <myers.t.scott@gmail.com>
2019-08-29 08:41:34 -0400

    HTML parsing complete. Should probably be tuned to be more modular in the future

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
  Enter a web page, python will parse it and generate a word cloud
  
  The steps for development right now will be:
- 1. Get local data set
- 2. Word cloud from local set
- 3. Learn how to parse HTML in python
- 4. Integrate two together
+1. HTML Parse [ DONE ]
+2. Generate word cloud
+3. Integration of the two systems
 
+
+Refernce in using BeautifulSoup came from [Hlab](https://blog.hlab.tech/learn-how-to-do-web-scraping-using-python-3-7-and-beautiful-soup-4/)
--- html_parse.py
+++ html_parse.py
@@ -1,25 +1,21 @@
-from html.parser import HTMLParser
-import urllib
-from BeautifulSoup import BeautifulSoup
+import urllib3
+from bs4 import BeautifulSoup
 
 
-#----------- HTMLParser implementation
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+# Beautiful Soup implementation   -- ** Make sure ToS of website allow scraping**
+# Also using urllib3
+# -------------------------
+http = urllib3.PoolManager()
+#### ENTER URL BELOW
+url = 'http://'
+response = http.request('GET', url)
+soup = BeautifulSoup(response.data, features="html.parser")
 
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+# Posts variable to search for specific HTML class. Tune to specific website
+posts = soup.findAll(class_='blog-entry-content')
 
-    def handle_data(self, data):
-        print("Encountered some data  :", data)]
-#------------------------------
-
-
-#Beautiful Soup implementation   -- ** Make sure ToS of website allow scraping**
-#-------------------------
-page = urllib.urlopen('www.website.com')
-soup = BeautifulSoup(page)
-
-x = soup.body.find('div', attrs={'class' : 'container'}).text
-#--------------------------
+for post in posts:
+    title = post.find(class_='blog-entry-title entry-title').get_text().replace('\n', '')
+    link = post.find('a')['href']
+    date = post.select('.blog-entry-date.clr')[0].get_text()
+    print(title + link + date)
